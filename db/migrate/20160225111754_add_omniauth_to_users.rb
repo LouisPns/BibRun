8e4570e9e638738b2a387d517fde24a10bb734dd
louis pons <louis.pons@icloud.com>
2016-02-25 14:29:10 +0100

    Facebook Connect is working

--- db/migrate/20160225111754_add_omniauth_to_users.rb
+++ db/migrate/20160225111754_add_omniauth_to_users.rb
@@ -1,9 +1,5 @@
 class AddOmniauthToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :rails, :string
-    add_column :users, :g, :string
-    add_column :users, :migration, :string
-    add_column :users, :AddOmniauthToUsers, :string
     add_column :users, :provider, :string
     add_column :users, :uid, :string
     add_column :users, :token, :string
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160225061149) do
+ActiveRecord::Schema.define(version: 20160225111754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,11 +70,6 @@ ActiveRecord::Schema.define(version: 20160225061149) do
     t.string   "picture"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "photo"
-    t.string   "rails"
-    t.string   "g"
-    t.string   "migration"
-    t.string   "AddOmniauthToUsers"
     t.string   "provider"
     t.string   "uid"
     t.string   "token"
